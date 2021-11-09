19f449e7d170c4d5bf81a57a1d91688f966d3752
swordfish <swordfish@localhost>
2021-11-09 22:20:02 +0530

    First commit to add test cases

---
+++ Test Suites/Day1/TestSuite1.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>bf434830-7d4f-49ef-b8bc-7352b3fe5d8a</testSuiteGuid>
+   <testCaseLink>
+      <guid>2b6ff744-95ff-4b5d-a558-a473f648c9e2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Day1/TestCase1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f63f8cbb-a9b6-49bd-b2a1-e2a53360217f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Day1/TestCase2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
